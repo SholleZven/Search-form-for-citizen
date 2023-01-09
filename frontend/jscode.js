9acf238bf90d0d2a53947fe2b0a1d712bfd81f48
SholleZven <43926032+SholleZven@users.noreply.github.com>
2023-01-09 17:42:54 +0500

    Js update 2.0

--- frontend/jscode.js
+++ frontend/jscode.js
@@ -4,18 +4,40 @@ var family = form.querySelector(".family");
 var firstName = form.querySelector(".name");
 var dad_name = form.querySelector(".dad_name");
 var tel = form.querySelector(".tel");
-var big_text = form.querySelector(".big_text");
+var text = form.querySelectorAll(".text");
 var fields = form.querySelectorAll(".field");
+var shields = form.querySelectorAll(".text, .big_text");
+var array = ["Фамилия", "Имя", "Отчество", "Телефон", "Обращение"];
 
-validateBtn.addEventListener("click", function () {
-  for (var i = 0; i < fields.length; i++) {
+var generateError = function (text) {
+  var error = document.createElement("div");
+  error.className = "error";
+  error.style.color = "red";
+  error.innerHTML = text;
+  return error;
+};
+
+var removeValidation = function () {
+  var errors = form.querySelectorAll(".error");
+  for (var i = 0; i < errors.length; i++) {
+    errors[i].remove();
+    console.clear();
+  }
+};
+
+var checkFieldsPresence = function () {
+  for (var i = 0; i < shields.length; i++) {
     if (!fields[i].value) {
-      console.log("field is blank", fields[i]);
-      var error = document.createElement("div");
-      error.className = "error";
-      error.style.color = "red";
-      error.innerHTML = "Cannot be blank";
-      fields[i].parentElement.appendChild(error);
+      var error = generateError("Поле незаполнено");
+      shields[i].appendChild(error);
+      console.log("field: " + array[i] + " is blank");
+    } else {
+      console.log(array[i] + ": " + fields[i].value);
     }
   }
+};
+
+validateBtn.addEventListener("click", function () {
+  removeValidation();
+  checkFieldsPresence();
 });
